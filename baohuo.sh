c4d42294f52aa6925292cfe0aec072087736f77c
heymenshan-tech <heymenshan@gmail.com>
2025-04-29 09:35:18 +0800

    Update baohuo.sh

--- baohuo.sh
+++ baohuo.sh
@@ -1,15 +1,13 @@
-#!/bin/bash
-
-sudo apt install xdotool
+#!/system/bin/sh
 
 while true
 do
-  # 21~30 사이의 랜덤 초 생성
+  # 21~30초 랜덤 시간 만들기
   sleep_time=$((21 + RANDOM % 10))
   
   # 대기
   sleep "$sleep_time"
   
   # 엔터키 입력
-  xdotool key Return
+  input keyevent 66
 done
